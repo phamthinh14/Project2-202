72f272bfd226c72d075fc5914cbf16ec5fd0b635
phamthinh14 <phamthinh1994@gmail.com>
2019-10-01 23:58:56 -0400

    Load Textfile Successfully

--- Game.cpp
+++ Game.cpp
@@ -5,10 +5,11 @@
 #include "Game.h"
 #include <fstream>
 #include <string>
-#include <sstream>
+//#include <sstream>
 
 Game::Game() {
     int count = 0;
+    char comma = ',';
     string line;
     string m_name; //Name of the Foundable
     string m_type; //Type of the Foundable
@@ -16,11 +17,7 @@ Game::Game() {
     string tempRarity;
     int m_toughness; //Toughness of the Foundable
     string tempToughness;
-    string tempArray[577];
 
-//    fstream inputStream;
-//
-//    inputStream.open(FILE_NAME);
     ifstream theFile(FILE_NAME);
     if (!theFile.is_open()) {
         cout << " Failed to open" << endl;
@@ -28,24 +25,39 @@ Game::Game() {
         cout << "Opened OK" << endl;
     }
 
-    while (getline(theFile, line)) {
-        stringstream ss(line);
-        getline(ss, m_name, ',');
-        getline(ss, m_type, ',');
-        getline(ss, tempRarity, ',');
+    while (theFile.good()) {
+        getline(theFile, m_name, comma);
+        getline(theFile, m_type, comma);
+        getline(theFile, tempRarity, comma);
         m_rarity = stoi(tempRarity);
-        getline(ss, tempToughness, ',');
+        getline(theFile, tempToughness, '\n');
         m_toughness = stoi(tempToughness);
-        m_rarity = stoi(tempRarity);
         Foundable tempFoundable(m_name, m_type, m_rarity, m_toughness);
         m_allFoundables[count] = tempFoundable;
-        count++;
+        ++count;
     }
 
+
+
+//    while (getline(theFile, line)) {
+//        stringstream ss(line);
+//        getline(ss, m_name, ',');
+//        getline(ss, m_type, ',');
+//        getline(ss, tempRarity, ',');
+//        m_rarity = stoi(tempRarity);
+//        getline(ss, tempToughness, ',');
+//        m_toughness = stoi(tempToughness);
+//        m_rarity = stoi(tempRarity);
+//        Foundable tempFoundable(m_name, m_type, m_rarity, m_toughness);
+//        m_allFoundables[count] = tempFoundable;
+//        count++;
+//    }
+
     cout << count << endl;
-    for (int i = 0; i < count; ++i) {
-        cout << m_allFoundables[i].GetName() << " " << m_allFoundables[i].GetType() << " "
+    for (int i = 0; i < NUM_FOUNDABLE; ++i) {
+        cout << i << "/ " << m_allFoundables[i].GetName() << " " << m_allFoundables[i].GetType() << " "
              << m_allFoundables[i].GetRarity() << " " << m_allFoundables[i].GetToughness() << endl;
+
     }
 
     theFile.close();
